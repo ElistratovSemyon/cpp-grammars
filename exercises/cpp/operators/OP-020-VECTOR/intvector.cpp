fd74f57907cf6f8d09f3fc10459df25e82952351
ElistratovSemyon <57802917+ElistratovSemyon@users.noreply.github.com>
2020-02-15 22:04:17 +0300

    Update intvector.cpp
    Operator [] expand vector if i > size (module change)

--- exercises/cpp/operators/OP-020-VECTOR/intvector.cpp
+++ exercises/cpp/operators/OP-020-VECTOR/intvector.cpp
@@ -30,6 +30,23 @@ int IntVector::get(int i) const
 
 int & IntVector::operator [](int i)
 {
+    if (i >= size)
+    {
+        // expanding the vector, the resulting value is 0
+        int *tmp = new int[i+1];
+        for (int j = 0; j < size; j++)
+        {
+            tmp[j] = head[j];
+        }
+        for (int j = size; j < i + 1; j++)
+        {
+            tmp[j] = 0;
+        }
+        size = i + 1;
+        int * del = head;
+        head = tmp;
+        delete [] del;
+    }
     return head[i];
 }
 #endif
