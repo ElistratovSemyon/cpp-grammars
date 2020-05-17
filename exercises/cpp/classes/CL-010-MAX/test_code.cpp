a830e225230f3a89f21cfef26ea6c0a4f0c22c59
Elistratov Semyon <57802917+ElistratovSemyon@users.noreply.github.com>
2020-05-17 17:43:26 +0400

    Update test_code.cpp

--- exercises/cpp/classes/CL-010-MAX/test_code.cpp
+++ exercises/cpp/classes/CL-010-MAX/test_code.cpp
@@ -5,11 +5,11 @@
 using std::cout;
 using std::endl;
 
-void Empty_test();
+void Empty_test(MaxSeq & max);
 void Constructor_test();
-void Add_test();
-void Count_test();
-void Max_test();
+void Add_test(MaxSeq & max);
+void Count_test(MaxSeq & max);
+void Max_test(MaxSeq & max);
 
 
 int main(){
@@ -19,13 +19,15 @@ int main(){
     {
         Constructor_test();
         cout << "Constructor test completed succesfully." << endl;
-        Add_test();
+        MaxSeq max;
+        Add_test(max);
         cout << "'Add' test completed succesfully." << endl;
-        Max_test();
+        Max_test(max);
         cout << "'Max test' completed succesfully." << endl;
-        Count_test();
+        Count_test(max);
         cout << "'Count' test completed succesfully." << endl;
-        Empty_test();
+        MaxSeq new_max;
+        Empty_test(new_max);
         cout << "'Empty sequence' test completed succesfully." << endl;
         cout << "------------" << endl;
     }
@@ -44,51 +46,42 @@ int main(){
 void Constructor_test()
 {
     MaxSeq max;
-}                                                                                               
+}
 
-void Add_test()
-{                                                                         
-    MaxSeq max;                                                                                     
-    for (int i = 0; i < 10; i++)                                                                    
-    {                                                                                               
-        max.add(i);                                                                                 
-    }                                                                                               
+void Add_test(MaxSeq & max)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        max.add(i);
+    } 
+    max.add(1000);
+    max.add(10);
+    max.add(-15);
+    max.add(-10000);
+    max.add(1001);
+    max.add(10);
 } 
 
                                                                                                 
                                                                                                     
-void Max_test()
-{                                                                          
-    MaxSeq max;                                                                                     
-    max.add(1000);                                                                                  
-    max.add(10);                                                                                    
-    max.add(-15);                                                                                   
-    max.add(-10000);                                                                                
-    max.add(1001);                                                                                  
-    max.add(10);                                                                                    
+void Max_test(MaxSeq & max)
+{                                                                                                                                                             
     if (max.max() != 1001)
     {
         throw std::logic_error("Incorrectly computing max");
     }                                                                                                                                           
 }       
 
-void Count_test()
-{                                                                          
-    MaxSeq max;                                                                                     
-    max.add(10);                                                                                  
-    max.add(20);                                                                                    
-    max.add(3);                                                                                   
-    max.add(4);                                                                                
-                                                                                      
-    if (max.count() != 4)
+void Count_test(MaxSeq & max)
+{                                                                                                                                                     
+    if (max.count() != 16)
     {
         throw std::logic_error("Incorrectly computing count");
     }                                                                                                                                           
 } 
 
-void Empty_test()
+void Empty_test(MaxSeq & max)
 {
-    MaxSeq max;
     try
     {
         max.max();
