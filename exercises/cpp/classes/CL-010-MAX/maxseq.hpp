4addd18ef44c44a2429c1b7153354b418fcfb223
Elistratov Semyon <57802917+ElistratovSemyon@users.noreply.github.com>
2020-05-17 17:31:48 +0400

    Update maxseq.hpp

--- exercises/cpp/classes/CL-010-MAX/maxseq.hpp
+++ exercises/cpp/classes/CL-010-MAX/maxseq.hpp
@@ -1,15 +1,11 @@
 #ifndef UNIT
 #define UNIT
-#include <cstdio>
 class MaxSeq
 {
     int max_n;
     size_t count_n;
 public:
-    MaxSeq(){
-        max_n = 0;
-        count_n = 0;
-    }
+    MaxSeq();
     void add(int n);
     int max();
     size_t count();
