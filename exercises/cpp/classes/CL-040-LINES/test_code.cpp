be3a877cde663b6957b7e5bb5ee487c14fdd9970
Elistratov Semyon <57802917+ElistratovSemyon@users.noreply.github.com>
2020-05-20 22:41:23 +0400

    Update test_code.cpp

--- exercises/cpp/classes/CL-040-LINES/test_code.cpp
+++ exercises/cpp/classes/CL-040-LINES/test_code.cpp
@@ -1,42 +1,95 @@
 #include <iostream>
-#include <cstdio>
+#include <string>
 #include "textfilereader.hpp"
 
 using std::cout;
 using std::endl;
 using std::boolalpha;
 
-bool test_for_output_text();
-bool test_call_hasnext_at_end();
-bool test_empty_file();
+void Constructor_test();
 
-int main()
-{
-    cout << std::boolalpha << "1 test (output text): " <<  test_for_output_text() << endl;
-    cout << "2 test (test hasNext at end): " << test_call_hasnext_at_end() << endl;
-    cout << "3 test (empty file: " << test_empty_file() << endl;
+void Get_test(TextFileReader & f);
+void Has_next_test(TextFileReader & f);
+void Last_has_next_test(TextFileReader & f);
 
-    cout << "testing completed." << endl;
+void Empty_file_test(TextFileReader & f);
+
+
+int main(){
+    cout << "Start tests." << endl;
+    cout << "------------" << endl;
+    try
+    {
+        Constructor_test();
+        cout << "Constructor test completed succesfully." << endl;
+        TextFileReader f ("test_1.txt");
+        Has_next_test(f);
+        cout << "'hasNext' test (not eof) completed succesfully." << endl;
+        Get_test(f);
+        cout << "'Get' test completed succesfully." << endl;
+        Last_has_next_test(f);
+        cout << "'hasNext' test (eof) completed succesfully." << endl;
+        TextFileReader f_2 ("test_3.txt");
+        
+        Empty_file_test(f_2);
+        cout << "'Empty file' test completed succesfully." << endl;
+        cout << "------------" << endl;
+    }
+    catch(const std::exception & e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch(...)
+    {
+        cout << "Unexpected error.\n";
+    }
+    
     return 0;
 }
 
-bool test_for_output_text()
+void Constructor_test()
 {
-    const char * path = "test_1.txt";
-    TextFileReader f(path);
-    return ("1_line" == f.get()) && ("2_line" == f.get()) && (f.hasNext() == true);
+    TextFileReader x("test_2.txt");
 }
 
-bool test_call_hasnext_at_end()
+void Get_test(TextFileReader & f)
 {
-    const char * path = "test_2.txt";
-    TextFileReader f(path);
-    return ("1_line" == f.get()) && (f.hasNext() == false);
+    if (f.get() != "1_line")
+    {
+        throw std::logic_error("Wrong read from file");
+    }
+    f.get();
+    if (f.get() != "3_line")
+    {
+        throw std::logic_error("Wrong read from file");
+    }
+    f.get();
 }
 
-bool test_empty_file()
+void Has_next_test(TextFileReader & f)
 {
-    const char * path = "test_3.txt";
-    TextFileReader f(path);
-    return (f.hasNext() == false);
+    if (f.hasNext() == false)
+    {
+        throw std::logic_error("Wrong check for EOF");
+    }
+    
 }
+
+void Last_has_next_test(TextFileReader & f)
+{
+    if (f.hasNext() == true)
+    {
+        throw std::logic_error("Wrong check for EOF");
+    }
+    
+}
+
+void Empty_file_test(TextFileReader & f)
+{
+    cout << f.get();
+    if (f.hasNext() != false)
+    {
+        throw std::logic_error("Wrong check for EOF");
+    } 
+}
+
